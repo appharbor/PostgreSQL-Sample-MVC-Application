a56dd4e2201b81b6f234456ea45941d4f65dae03
Michael Friis <friism@gmail.com>
2011-12-15 18:26:41 -0800

    remove crud

--- JustOneDBExample/Global.asax.cs
+++ JustOneDBExample/Global.asax.cs
@@ -27,9 +27,9 @@ namespace JustOneDBExample
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
 			routes.MapRoute(
-				"Default", // Route name
-				"{controller}/{action}/{id}", // URL with parameters
-				new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+				"Default",
+				"{controller}/{action}/{id}",
+				new { controller = "Home", action = "Index", id = UrlParameter.Optional }
 			);
 		}
 
